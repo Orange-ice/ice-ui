39fa5d0193490ef97a430dda4678efc19fb6ada9
little_ice <2902978956@qq.com>
2020-08-05 10:20:27 +0800

    创建 button-group.vue

---
+++ src/button-group.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="g-button-group">
+        <slot></slot>
+    </div>
+</template>
+<script></script>
+<style lang="scss">
+    .g-button-group {
+        display: inline-flex;
+
+        > .g-button {
+            border-radius: 0;
+            margin-left: -1px;
+            &:first-child {
+                border-radius: var(--border-radius) 0 0 var(--border-radius);
+            }
+            &:last-child {
+                border-radius: 0 var(--border-radius) var(--border-radius) 0
+            }
+            &:hover {
+                position: relative;
+                z-index: 1
+            }
+        }
+    }
+</style>
