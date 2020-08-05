ceed86f13c540b4a08c3ee0d1fac17e9c34461fc
little_ice <2902978956@qq.com>
2020-08-05 08:06:46 +0800

    验证 iconPosition 只能为 left 或 right

--- src/button.vue
+++ src/button.vue
@@ -10,7 +10,16 @@
 </template>
 <script>
     export default {
-        props:['icon','iconPosition']
+        props:{
+            icon:{},
+            iconPosition:{
+                type:String,
+                default:'left',
+                validator(value){
+                    return value === 'left' || value === 'right'
+                }
+            }
+        }
     }
 </script>
 <style lang="scss">
