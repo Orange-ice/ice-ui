965e7c88356ade7c21640c4cbffb9099a7fe9a74
little_ice <2902978956@qq.com>
2020-08-05 09:54:53 +0800

    添加 click 事件

--- src/app.js
+++ src/app.js
@@ -2,9 +2,14 @@ import Vue from 'vue'
 import Button from './button.vue'
 import Icon from './icon.vue'
 
-Vue.component('g-button',Button)
-Vue.component('g-icon',Icon)
+Vue.component('g-button', Button)
+Vue.component('g-icon', Icon)
 
 new Vue({
-    el:'#app'
+    el: '#app',
+    data: {
+        loading1: false,
+        loading2: true,
+        loading3: false
+    }
 })
--- src/button.vue
+++ src/button.vue
@@ -1,7 +1,7 @@
 <template>
-    <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
-        <g-icon v-if="icon" :name="icon" class="icon"></g-icon>
-        <g-icon class="loading" name="loading"></g-icon>
+    <button class="g-button" :class="{[`icon-${iconPosition}`]:true}" @click="$emit('click')">
+        <g-icon v-if="icon && !loading" :name="icon" class="icon"></g-icon>
+        <g-icon class="loading icon" v-if="loading" name="loading"></g-icon>
         <div class="content">
             <slot></slot>
         </div>
@@ -11,6 +11,10 @@
     export default {
         props:{
             icon:{},
+            loading:{
+                type: Boolean,
+                default: false
+            },
             iconPosition:{
                 type:String,
                 default:'left',
