ba28564313975d460caeb637d1ec4556dcf21c1a
Burt <2902978956@qq.com>
2020-08-08 18:38:25 +0800

    添加 input.vue 的测试用例

--- src/input.vue
+++ src/input.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="wrapper" :class="{error}">
-    <input type="text" :value="value" :disabled="disabled" :readonly="readonly">
+    <input type="text" :value="value" :disabled="disabled" :readonly="readonly"
+           @change="$emit('change',$event)"
+           @input="$emit('input',$event)"
+           @blur="$emit('blur',$event)"
+           @focus="$emit('focus',$event)">
     <template v-if="error">
       <icon name="error" class="icon-error"></icon>
       <span class="errorMessage">{{ error }}</span>
