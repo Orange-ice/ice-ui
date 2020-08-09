61520b6553692a0c4dc8d086a19566706bbfa563
Burt <2902978956@qq.com>
2020-08-09 11:24:36 +0800

    添加可设置 gutter offset 功能

--- src/col.vue
+++ src/col.vue
@@ -1,28 +1,42 @@
 <template>
-  <div class="col" :class="[`col-${span}`]">
-    <slot></slot>
+  <div class="col" :class="[`col-${span}`, offset && `offset-${offset}`]"
+       :style="{paddingLeft: gutter/2 + 'px',paddingRight: gutter/2 + 'px'}">
+    <div style="border: 1px solid green;height:100px">
+      <slot></slot>
+    </div>
   </div>
 </template>
 <script>
 export default {
   name: 'IceCol',
+  data() {
+    return {
+      gutter: 0
+    }
+  },
   props: {
     span: {
       type: [Number, String]
-    }
+    },
+    offset: {
+      type: [Number, String]
+    },
   }
 }
 </script>
 <style lang="scss" scoped>
 .col {
-  height: 100px;
-  background: gray;
   width: 50%;
-  border: solid 1px blue;
   $class-prefix: col-;
   @for $n from 1 through 24 {
     &.#{$class-prefix}#{$n} {
       width: ($n/24)*100%;
+    }
+  }
+  $class-prefix: offset-;
+  @for $n from 1 through 24 {
+    &.#{$class-prefix}#{$n} {
+      margin-left: ($n/24)*100%;
     }
   }
 }
--- src/row.vue
+++ src/row.vue
@@ -1,10 +1,27 @@
 <template>
-  <div class="row">
+  <div class="row" :style="{marginLeft:-gutter/2 + 'px',marginRight:-gutter/2 + 'px'}">
     <slot></slot>
   </div>
 </template>
 <script>
-export default {}
+export default {
+  name:'IceRow',
+  props:{
+    gutter:{
+      type:[Number,String]
+    }
+  },
+  mounted() {
+    this.$children.forEach((vm)=>{
+      vm.gutter = this.gutter
+    })
+  }
+}
+const div = document.createElement('div');
+const childDiv = document.createElement('div');
+
+div.appendChild(childDiv)
+document.body.appendChild(div)
 </script>
 <style lang="scss" scoped>
 .row{
