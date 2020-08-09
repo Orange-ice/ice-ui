7b24dd430bb73df2f53c7d02538fed167c900923
Burt <2902978956@qq.com>
2020-08-09 10:52:15 +0800

    初步实现 row 和 col

--- src/app.js
+++ src/app.js
@@ -3,12 +3,16 @@ import Button from './button.vue'
 import Icon from './icon.vue'
 import ButtonGroup from './button-group.vue'
 import Input from './input'
+import Col from './col'
+import Row from './row'
 
 
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
 Vue.component('g-button-group', ButtonGroup)
 Vue.component('g-input', Input)
+Vue.component('g-row',Row)
+Vue.component('g-col',Col)
 
 new Vue({
   el: '#app',
---
+++ src/col.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="col" :class="[`col-${span}`]">
+    <slot></slot>
+  </div>
+</template>
+<script>
+export default {
+  name: 'IceCol',
+  props: {
+    span: {
+      type: [Number, String]
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.col {
+  height: 100px;
+  background: gray;
+  width: 50%;
+  border: solid 1px blue;
+  $class-prefix: col-;
+  @for $n from 1 through 24 {
+    &.#{$class-prefix}#{$n} {
+      width: ($n/24)*100%;
+    }
+  }
+}
+</style>
