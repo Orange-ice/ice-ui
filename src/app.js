da5e108b42d9812695bc45b55c5a79c24164616a
Burt <2902978956@qq.com>
2020-08-11 14:25:05 +0800

    添加 toast 组件和 plugin.js

--- src/app.js
+++ src/app.js
@@ -10,6 +10,8 @@ import Header from './header'
 import Footer from './footer'
 import Sider from './sider'
 import Content from './content'
+import Toast from './toast'
+import plugin from './plugin.js'
 
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
@@ -22,6 +24,8 @@ Vue.component('g-header',Header)
 Vue.component('g-sider',Sider)
 Vue.component('g-content',Content)
 Vue.component('g-footer',Footer)
+Vue.component('g-toast',Toast)
+Vue.use(plugin)
 
 new Vue({
   el: '#app',
@@ -32,8 +36,8 @@ new Vue({
     message:'hello'
   },
   methods:{
-    inputChange(e){
-      console.log(e)
+    showToast(){
+      this.$toast('我是 message')
     }
   }
 })
---
+++ src/toast.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="toast">
+    <slot></slot>
+  </div>
+</template>
+<script>
+export default {
+  name: 'IceToast'
+}
+</script>
+<style lang="scss" scoped>
+$font-size: 14px;
+$toast-height: 40px;
+$toast-bg: rgba(0, 0, 0, 0.75);
+.toast {
+  position: fixed;top: 0;left: 50%;transform: translateX(-50%);
+  font-size: $font-size;color: #fff;height: $toast-height;line-height: 1.8;
+  padding: 0 16px;
+  display: flex;align-items: center;
+  background: $toast-bg;
+  border-radius: 4px;
+  box-shadow: 0 0 3px 0 rgba(0, 0, 0, 0.5);
+}
+</style>
