e391ca3f40402fa8492d5033f856e74bca0f3cee
Burt <2902978956@qq.com>
2020-08-15 17:26:13 +0800

    完成官网的 Button 页面

---
+++ docs/.vuepress/components/button-demos.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <g-button>默认按钮</g-button>
+    <g-button icon="settings">默认按钮</g-button>
+    <g-button :loading="loading" @click="loading=!loading">默认按钮</g-button>
+  </div>
+</template>
+<script>
+import Button from '../../../src/button'
+export default {
+  components:{'g-button':Button},
+  data(){
+    return{
+      loading:true
+    }
+  }
+}
+</script>
--- docs/components/button.md
+++ docs/components/button.md
@@ -3,4 +3,18 @@ title: Button 按钮
 ---
 # Button 按钮
 
-使用方法
+**使用方法**
+
+基本用法：
+
+<ClientOnly>
+<button-demos></button-demos>
+</ClientOnly>
+
+示例代码：
+
+```vue
+<g-button>默认按钮</g-button>
+<g-button icon="settings">默认按钮</g-button>
+<g-button :loading="loading" @click="loading=!loading">默认按钮</g-button>
+```
