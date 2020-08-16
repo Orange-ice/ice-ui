6ce21e148e208e4ecb28c6ca8e32e4f90b429683
Burt <2902978956@qq.com>
2020-08-16 12:45:47 +0800

    完成官网的 layout 页面

---
+++ docs/.vuepress/components/layout-demo-1.vue
@@ -0,0 +1,49 @@
+<template>
+  <div style="padding-top: 16px;">
+    <g-layout style="color: white; margin-bottom:50px;">
+      <g-header style="height: 50px; background:lightskyblue;">
+        header
+      </g-header>
+      <g-content style="height: 100px; background:deepskyblue;">
+        content
+      </g-content>
+      <g-footer style="height: 50px; background:lightskyblue;">
+        footer
+      </g-footer>
+    </g-layout>
+  </div>
+</template>
+<style scoped>
+* {
+  box-sizing: border-box;
+}
+</style>
+<script>
+import GLayout from '../../../src/layout'
+import GHeader from '../../../src/header'
+import GFooter from '../../../src/footer'
+import GContent from '../../../src/content'
+import GSider from '../../../src/sider'
+
+export default {
+  components: {GLayout, GHeader, GFooter, GContent, GSider},
+  data () {
+    return {
+      content: `
+          <g-layout style="color: white; margin-bottom:50px;">
+            <g-header style="height: 50px; background:lightskyblue;">
+              header
+            </g-header>
+            <g-content style="height: 100px; background:deepskyblue;">
+              content
+            </g-content>
+            <g-footer style="height: 50px; background:lightskyblue;">
+              footer
+            </g-footer>
+          </g-layout>
+
+      `.replace(/^ {8}/gm, '').trim()
+    }
+  }
+}
+</script>
