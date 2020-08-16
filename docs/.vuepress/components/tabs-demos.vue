0d307eb185e545a146701db502c438cb320265d5
Burt <2902978956@qq.com>
2020-08-16 10:12:34 +0800

    完成官网的 tabs 页面

---
+++ docs/.vuepress/components/tabs-demos.vue
@@ -0,0 +1,54 @@
+<template>
+  <div style="margin-top: 10px">
+    <div>
+      <g-tabs :selected="selected1">
+        <g-tabs-head>
+          <g-tabs-item name="1">标题一</g-tabs-item>
+          <g-tabs-item name="2">标题二</g-tabs-item>
+        </g-tabs-head>
+        <g-tabs-body>
+          <g-tabs-pane name="1">内容1</g-tabs-pane>
+          <g-tabs-pane name="2">内容2</g-tabs-pane>
+        </g-tabs-body>
+      </g-tabs>
+    </div>
+
+    <div style="margin-top: 20px">
+      <g-tabs :selected="selected2">
+        <g-tabs-head>
+          <g-tabs-item name="1">标题一</g-tabs-item>
+          <g-tabs-item disabled name="2">标题二</g-tabs-item>
+          <g-tabs-item name="3">标题三</g-tabs-item>
+        </g-tabs-head>
+        <g-tabs-body>
+          <g-tabs-pane name="1">内容1</g-tabs-pane>
+          <g-tabs-pane name="2">内容2</g-tabs-pane>
+          <g-tabs-pane name="3">内容3</g-tabs-pane>
+        </g-tabs-body>
+      </g-tabs>
+    </div>
+  </div>
+</template>
+<script>
+import Tabs from '../../../src/tabs'
+import TabsHead from '../../../src/tabs-head'
+import TabsBody from '../../../src/tabs-body'
+import TabsItem from '../../../src/tabs-item'
+import TabsPane from '../../../src/tabs-pane'
+
+export default {
+  data() {
+    return {
+      selected1: '1',
+      selected2: '1'
+    }
+  },
+  components: {
+    'g-tabs': Tabs,
+    'g-tabs-head': TabsHead,
+    'g-tabs-body': TabsBody,
+    'g-tabs-item': TabsItem,
+    'g-tabs-pane': TabsPane,
+  }
+}
+</script>
--- docs/components/tabs.md
+++ docs/components/tabs.md
@@ -2,3 +2,34 @@
 title: Tabs 标签页
 ---
 # Tabs 标签页
+
+**使用方法**
+
+#### 基本用法
+
+<ClientOnly>
+<tabs-demos></tabs-demos>
+</ClientOnly>
+
+#### 示例代码
+
+```vue
+data() {
+    return {
+      selected: '1',
+    }
+},
+
+<g-tabs :selected="selected">
+    <g-tabs-head>
+      <g-tabs-item name="1">标题一</g-tabs-item>
+      <g-tabs-item disabled name="2">标题二</g-tabs-item>
+      <g-tabs-item name="3">标题三</g-tabs-item>
+    </g-tabs-head>
+    <g-tabs-body>
+      <g-tabs-pane name="1">内容1</g-tabs-pane>
+      <g-tabs-pane name="2">内容2</g-tabs-pane>
+      <g-tabs-pane name="3">内容3</g-tabs-pane>
+    </g-tabs-body>
+</g-tabs>
+```
