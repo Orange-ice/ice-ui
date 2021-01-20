6c490bfd98ed2485938db6d0061e422a133fa0e3
Burt <2902978956@qq.com>
2021-01-20 21:00:42 +0800

    parcel 迁移到 vue-cli

---
+++ src/demo.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <c-button>安妮</c-button>
+  </div>
+</template>
+
+<script>
+import button from "./button";
+export default {
+  name: 'demo',
+  components: {
+    'c-button': button
+  }
+}
+</script>
