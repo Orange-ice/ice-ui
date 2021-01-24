63ddebb0b17f033a65b54bde67cd20ab62903809
Burt <2902978956@qq.com>
2021-01-24 13:42:24 +0800

    粗糙的渲染数据

---
+++ src/cascader/cascader.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="cascader">
+    <div class="trigger">
+      <slot></slot>
+    </div>
+    <div class="popover">
+      <div v-for="item in source">
+        <cascader-item :source-item="item" />
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import cascaderItem from '@/cascader/cascaderItem.vue'
+export default {
+  name: 'Cascader',
+  components: { cascaderItem },
+  props: {
+    source: {
+      type: Array
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/cascader/cascaderItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    {{sourceItem.name}}
+    <cascader-item
+      v-for="item in sourceItem.children"
+      v-if="sourceItem.children"
+      :source-item="item"
+    ></cascader-item>
+  </div>
+</template>
+
+<script>
+const cascaderItem = {
+  name: 'cascaderItem',
+  components: {
+    cascaderItem: cascaderItem
+  },
+  props: {
+    sourceItem: {
+      type: Object
+    }
+  }
+}
+export default cascaderItem
+</script>
+
+<style lang="scss" scoped></style>
